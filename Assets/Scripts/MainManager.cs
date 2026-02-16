764120d19987dabcb7d4544dfa8056bcb183b569
psathvick <psathvick@gmail.com>
2026-02-16 18:25:35 +0530

    color changes during sessions implemented
    json file concept added

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.IO;
 
 public class MainManager : MonoBehaviour
 {
@@ -15,5 +16,34 @@ public class MainManager : MonoBehaviour
         }
         Instance = this;
         DontDestroyOnLoad(gameObject);
+        LoadColor();
+    }
+
+    [System.Serializable]
+
+    class SaveData
+    {
+        public Color teamColor;
+    }
+
+    public void SaveColor()
+    {
+        SaveData data = new SaveData();
+        data.teamColor = teamColor;
+
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+
+    }
+
+    public void LoadColor()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if(File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+            teamColor = data.teamColor;
+        }
     }
 }
--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -24,6 +24,7 @@ public class MenuUIHandler : MonoBehaviour
         ColorPicker.Init();
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         ColorPicker.onColorChanged += NewColorSelected;
+        ColorPicker.SelectColor(MainManager.Instance.teamColor);
     }
 
     public void StartGame()
@@ -38,5 +39,17 @@ public class MenuUIHandler : MonoBehaviour
         #else
         Application.Quit();
         #endif
+        MainManager.Instance.SaveColor();
+    }
+
+    public void SaveColorClicked()
+    {
+        MainManager.Instance.SaveColor();
+    }
+
+    public void LoadColorClicked()
+    {
+        MainManager.Instance.LoadColor();
+        ColorPicker.SelectColor(MainManager.Instance.teamColor);
     }
 }
